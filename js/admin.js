32502c3844a2f6f01ba9637881f079dafeae4bd0
Josacha <120397811+Josacha@users.noreply.github.com>
2026-03-13 13:11:52 -0600

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,99 +1,32 @@
-import { db, auth } from "./firebase.js";
+import { db } from "./firebase.js";
 
 import {
 collection,
-addDoc,
-getDocs,
-deleteDoc,
-doc
+addDoc
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-import {
-signInWithEmailAndPassword,
-onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+document.getElementById("guardar").onclick = async () => {
 
+const producto = {
 
-const loginBtn=document.getElementById("loginBtn");
+nombre: document.getElementById("nombre").value,
 
-loginBtn.onclick=async()=>{
+marca: document.getElementById("marca").value,
 
-const email=document.getElementById("email").value;
-const pass=document.getElementById("password").value;
+categoria: document.getElementById("categoria").value,
 
-await signInWithEmailAndPassword(auth,email,pass);
+voltaje: document.getElementById("voltaje").value,
 
-};
+amperaje: document.getElementById("amperaje").value,
 
+imagen: document.getElementById("imagen").value,
 
-onAuthStateChanged(auth,user=>{
-
-if(user){
-
-document.getElementById("login").style.display="none";
-document.getElementById("panel").style.display="block";
-
-cargar();
-
-}
-
-});
-
-
-async function cargar(){
-
-const lista=document.getElementById("lista");
-
-lista.innerHTML="";
-
-const snapshot=await getDocs(collection(db,"productos"));
-
-snapshot.forEach(d=>{
-
-const p=d.data();
-
-const div=document.createElement("div");
-
-div.innerHTML=`
-
-${p.nombre}
-
-<button onclick="eliminar('${d.id}')">
-Eliminar
-</button>
-
-`;
-
-lista.appendChild(div);
-
-});
-
-}
-
-
-window.eliminar=async(id)=>{
-
-await deleteDoc(doc(db,"productos",id));
-
-cargar();
+descripcion: document.getElementById("descripcion").value
 
 };
 
+await addDoc(collection(db, "productos"), producto);
 
-document.getElementById("guardar").onclick=async()=>{
-
-const nombre=document.getElementById("nombre").value;
-const categoria=document.getElementById("categoria").value;
-const imagen=document.getElementById("imagen").value;
-
-await addDoc(collection(db,"productos"),{
-
-nombre,
-categoria,
-imagen
-
-});
-
-cargar();
+alert("Producto agregado");
 
 };
