3a8019365af5dcabb3475b239af86ee21c418507
Josacha <120397811+Josacha@users.noreply.github.com>
2026-03-13 18:36:30 -0600

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -2,31 +2,124 @@ import { db } from "./firebase.js";
 
 import {
 collection,
-addDoc
+addDoc,
+getDocs,
+deleteDoc,
+doc,
+updateDoc
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-document.getElementById("guardar").onclick = async () => {
+const tabla = document.getElementById("tablaProductos");
+
+let editandoId = null;
+
+async function cargarProductos(){
+
+tabla.innerHTML="";
+
+const querySnapshot = await getDocs(collection(db,"productos"));
+
+querySnapshot.forEach((docu)=>{
+
+const p = docu.data();
+const id = docu.id;
+
+tabla.innerHTML += `
+
+<tr>
+
+<td>
+<img src="${p.imagen}" width="60">
+</td>
+
+<td>${p.nombre}</td>
+
+<td>${p.marca}</td>
+
+<td>${p.categoria}</td>
+
+<td>
+
+<button onclick="editarProducto('${id}','${p.nombre}','${p.marca}','${p.categoria}','${p.voltaje}','${p.amperaje}','${p.imagen}','${p.descripcion}')">
+Editar
+</button>
+
+<button onclick="eliminarProducto('${id}')">
+Eliminar
+</button>
+
+</td>
+
+</tr>
+
+`;
+
+});
+
+}
+
+window.eliminarProducto = async(id)=>{
+
+if(confirm("Eliminar producto?")){
+
+await deleteDoc(doc(db,"productos",id));
+
+cargarProductos();
+
+}
+
+}
+
+window.editarProducto = (id,nombre,marca,categoria,voltaje,amperaje,imagen,descripcion)=>{
+
+editandoId = id;
+
+document.getElementById("nombre").value = nombre;
+document.getElementById("marca").value = marca;
+document.getElementById("categoria").value = categoria;
+document.getElementById("voltaje").value = voltaje;
+document.getElementById("amperaje").value = amperaje;
+document.getElementById("imagen").value = imagen;
+document.getElementById("descripcion").value = descripcion;
+
+window.scrollTo(0,0);
+
+}
+
+document.getElementById("guardar").onclick = async()=>{
 
 const producto = {
 
 nombre: document.getElementById("nombre").value,
-
 marca: document.getElementById("marca").value,
-
 categoria: document.getElementById("categoria").value,
-
 voltaje: document.getElementById("voltaje").value,
-
 amperaje: document.getElementById("amperaje").value,
-
 imagen: document.getElementById("imagen").value,
-
 descripcion: document.getElementById("descripcion").value
 
 };
 
-await addDoc(collection(db, "productos"), producto);
+if(editandoId){
+
+await updateDoc(doc(db,"productos",editandoId),producto);
+
+editandoId=null;
+
+alert("Producto actualizado");
+
+}else{
+
+await addDoc(collection(db,"productos"),producto);
 
 alert("Producto agregado");
 
-};
+}
+
+document.querySelectorAll(".admin-form input, .admin-form textarea").forEach(e=>e.value="");
+
+cargarProductos();
+
+}
+
+cargarProductos();
